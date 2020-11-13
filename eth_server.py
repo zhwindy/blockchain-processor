f2d24bd7804270f4da89836fb13bc5570a226b0c
zhaopengfei <zhaopengfei@coldlar.com>
2020-11-13 15:05:50 +0800

    fix api server

--- eth_server.py
+++ eth_server.py
@@ -188,7 +188,7 @@ def broadcast():
 
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 18760
+    host = "127.0.0.1"
+    port = 18788
     debug = False
     app.run(host=host, port=port, debug=debug)
