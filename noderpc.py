0060843e2bc775cd2a89dcb31fa84fe58f3dc611
zhwindy <kmust091314@gmail.com>
2023-03-08 13:42:10 +0800

    disable session

--- noderpc.py
+++ noderpc.py
@@ -4,7 +4,7 @@ from logger import logging
 
 NODE_ENDPOINT = "https://eth-mainnet.nodereal.io/v1/a4a9f892480d45e395f93945c4b77c6e"
 
-session = requests.Session()
+# session = requests.Session()
 
 
 def demo_get_block_number(node_url=None):
@@ -14,8 +14,8 @@ def demo_get_block_number(node_url=None):
     try:
         url = node_url if node_url else NODE_ENDPOINT
         data = {"jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 1}
-        # res = requests.post(url, json=data)
-        res = session.post(url, json=data)
+        res = requests.post(url, json=data)
+        # res = session.post(url, json=data)
         result = res.json()
     except Exception as e:
         logging.info(e)
@@ -30,8 +30,8 @@ def demo_get_block_by_number(block_number, node_url=None):
     url = node_url if node_url else NODE_ENDPOINT
     data = {"jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": [block_number, True], "id": 1}
     try:
-        # res = requests.post(url, json=data)
-        res = session.post(url, json=data)
+        res = requests.post(url, json=data)
+        # res = session.post(url, json=data)
         result = res.json()
     except Exception as e:
         logging.info(f"get block: {block_number} error: {e}")
@@ -51,8 +51,8 @@ def demo_get_transaction_receipt(txid, node_url=None):
     }
     url = node_url if node_url else NODE_ENDPOINT
     try:
-        # res = requests.post(url, json=data)
-        res = session.post(url, json=data)
+        res = requests.post(url, json=data)
+        # res = session.post(url, json=data)
         result = res.json()
     except Exception as e:
         logging.info(f"get tx: {txid} error: {e}")
